4d27418d7f45817f0db22d18f59eafaa7f9417af
debrayce <debrayce@LP271957.cib.net>
2018-02-08 15:31:37 +0100

    First modif AUGCMA

--- AUGCMA_TST/src/main/constantes/Constantes.java
+++ AUGCMA_TST/src/main/constantes/Constantes.java
@@ -20,9 +20,7 @@ public class Constantes {
 	//////////////////////////////////////////////////// INFORMATIONS POUR LES TESTS ////////////////////////////////////////////////////////////
 	public static final String URL_GOOGLE = PropertiesOutil.getInfoConstante("URL_TEST");
 	public static final String TITRE_PAGE = "Google";
-	public static final String CELIA_SAMY = PropertiesOutil.getInfoConstante("URL_CELIA");
-	public static final String FABIEN_URL_SAMY = PropertiesOutil.getInfoConstante("URL_SAMY");
-	public static final String FABIEN_TITRE_SAMY = "Instant";
-	public static final String URL_SAMY_FATIMA = PropertiesOutil.getInfoConstante("URL_SAMY");
-	public static final String TITRE_PAGE_SAMY = "Instant";
+
+	public static final String URL_APP_AUGCMA = PropertiesOutil.getInfoConstante("URL_AUGCMA");
+	public static final String TITRE_PAGE_AUGCMA = "nxs - Augmentation de CMA - BL ${baseline number}";
 }
---
+++ AUGCMA_TST/src/test/java/SC01Login.java
@@ -0,0 +1,52 @@
+package test.java;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+import constantes.Actions;
+import exceptions.SeleniumException;
+import main.bean.CasEssaiModeleBean;
+import main.constantes.Cibles;
+import main.constantes.Constantes;
+import moteurs.FirefoxImpl;
+import moteurs.GenericDriver;
+import outils.SeleniumOutils;
+
+public class SC01Login extends SC00Modele {
+
+       /**
+       * Id de serialisation.
+       */
+       private static final long serialVersionUID = 1L;
+
+       @Test
+       public void accesTest() throws SeleniumException {
+              
+              //Description du scénario
+              CasEssaiModeleBean scenario0 = new CasEssaiModeleBean();
+              
+              ///////////////////////////////////////////////////Configuration////////////////////////////////////////////////
+           SeleniumOutils outil = obtenirDriver(scenario0);          
+           
+              ///////////////////////////////////////////////////EXECUTION////////////////////////////////////////////////
+              String url = Constantes.URL_APP_AUGCMA;
+              String titre = Constantes.TITRE_PAGE_AUGCMA;
+
+              // Accès à google
+              outil.chargerUrl(url);
+              
+              // Attente de l'affichage du titre de la page
+              outil.attendreChargementPage(titre);
+              
+              	// Remplir l identite 
+      			//outil.action(Actions.SELECTIONNER, Cibles.SELECTION_CIVILITE_SAMY, "HOMME"); 
+      			//outil.action(Actions.VIDER_ET_SAISIR, Cibles.SELECTION_NOM_SAMY, "LEMONSIEUR"); 
+      			//outil.action(Actions.VIDER_ET_SAISIR, Cibles.SELECTION_PRENOM_SAMY, "KEVIN"); 
+      			
+
+       }
+    
+}
